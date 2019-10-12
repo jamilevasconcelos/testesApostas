afad2032587cae9542ecffca2d917f5842864d4d
Jamile <jamilevasconcelos@gmail.com>
2019-10-12 09:12:54 -0300

    teste maior lance crescente
    Ordena lista de forma crescente e retorna lance de maior valor

--- src/Avaliador.php
+++ src/Avaliador.php
@@ -4,4 +4,24 @@ namespace Jamile\TestesApostas;
 
 class Avaliador{
 
+
+        // Função auxiliar
+        // Compara valores de lances para ordenar listas (crescente)
+        function comparaLancesCrescente($lance1, $lance2) { 
+            return $lance1->getValor() > $lance2->getValor(); 
+        }
+
+        // Ordena lista de lances de forma crescente e retorna maior lance
+        function buscaMaiorLanceCrescente ($listaDeLances){      
+            $maiorlance = -1;
+            
+            usort($listaDeLances, array($this, 'comparaLancesCrescente')); //ordena lista
+            foreach ($listaDeLances as $lance) {
+                if ($lance->getValor() > $maiorlance) 
+                    $maiorlance = $lance->getValor();            
+            }
+            return $maiorlance;
+        }
+
+
 }
