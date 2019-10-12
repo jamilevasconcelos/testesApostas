f3d9dc15f957cfd37e0cde025ce371b47d58cee7
Jamile <jamilevasconcelos@gmail.com>
2019-10-12 09:33:37 -0300

    index
    Código incluído no index para facilitar testes pelo professor.
    Conteúdo deixado comentado.

--- index.php
+++ index.php
@@ -5,3 +5,47 @@ use Jamile\TestesApostas\Model\Usuario;
 use Jamile\TestesApostas\Avaliador;
 
 require 'vendor/autoload.php';
+
+
+// $leilao1 = new Leilao();
+
+// $usuario1 = new Usuario();
+// $lance1 = new Lance ();
+// $leilao1->setCodigo(1);
+// $usuario1->setCodigo(1);
+
+// $lance1->setCodigo(1);
+// $lance1->setCodLeilao(1);
+// $lance1->setCodUsuario(1);
+// $lance1->setValor(12);
+
+// $lance2 = new Lance ();
+// $usuario2 = new Usuario();
+
+// $usuario2->setCodigo(1);
+// $lance2->setCodLeilao(1);
+// $lance2->setCodigo(2);
+// $lance2->setCodUsuario(2);
+// $lance2->setValor(11);
+
+// $lance3 = new Lance ();
+// $usuario3 = new Usuario();
+
+// $usuario3->setCodigo(1);
+// $lance3->setCodLeilao(1);
+// $lance3->setCodigo(3);
+// $lance3->setCodUsuario(3);
+// $lance3->setValor(282);
+
+
+// $lances = [$lance1, $lance2, $lance3]; 
+
+// $avaliador = new Avaliador();
+// $maiorlance = $avaliador->buscaMaiorLance($lances);
+// $menorlance = $avaliador->buscaMenorLance($lances);
+
+// echo ("Bem-vindo ao sistema de Apostas!") . "\n";
+// echo ("Leilão: ") . $leilao1->getCodigo() . "\n";
+// echo ("Maior lance: ") . $maiorlance . "\n";
+// echo ("Menor lance: ") . $menorlance . "\n";
+
